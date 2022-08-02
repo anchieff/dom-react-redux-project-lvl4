b866f676aaa3a28e99f2026026006b2e98b01320
anchieff <info@annanekrasova.ru>
2022-08-02 17:15:44 +0400

    add float label

--- frontend/src/SigninPage.jsx
+++ frontend/src/SigninPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { Button, Form, Card, Row, Col } from "react-bootstrap";
+import { Button, Form, Card, Row, Col, FloatingLabel } from "react-bootstrap";
 import axios from "axios";
 import routes from "./routes";
 import useAuth from "./hooks";
@@ -86,65 +86,79 @@ const SigninPage = () => {
             <Card.Title>{t("signin.header")}</Card.Title>
             <Form onSubmit={formik.handleSubmit}>
               <Form.Group className="mt-3">
-                <Form.Control
-                  placeholder={t("signin.username")}
-                  name="username"
-                  autoComplete="username"
-                  required
-                  id="username"
-                  ref={userInput}
-                  isInvalid={
-                    (formik.touched.username && formik.errors.username) ||
-                    signinError
-                  }
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  value={formik.values.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {formik.errors.username}
-                </Form.Control.Feedback>
-                {signinError && (
+                <FloatingLabel
+                  label={t("signin.username")}
+                  controlId="username"
+                >
+                  <Form.Control
+                    placeholder={t("signin.username")}
+                    name="username"
+                    autoComplete="username"
+                    required
+                    ref={userInput}
+                    isInvalid={
+                      (formik.touched.username && formik.errors.username) ||
+                      signinError
+                    }
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                  />
                   <Form.Control.Feedback type="invalid">
-                    {t("signin.user_exist")}
+                    {formik.errors.username}
                   </Form.Control.Feedback>
-                )}
+                  {signinError && (
+                    <Form.Control.Feedback type="invalid">
+                      {t("signin.user_exist")}
+                    </Form.Control.Feedback>
+                  )}
+                </FloatingLabel>
               </Form.Group>
               <Form.Group className="mt-3">
-                <Form.Control
-                  placeholder={t("signin.password")}
-                  name="password"
-                  required
-                  id="password"
-                  type="password"
-                  isInvalid={formik.touched.password && formik.errors.password}
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  value={formik.values.password}
-                />
+                <FloatingLabel
+                  label={t("signin.password")}
+                  controlId="password"
+                >
+                  <Form.Control
+                    placeholder={t("signin.password")}
+                    name="password"
+                    required
+                    type="password"
+                    isInvalid={
+                      formik.touched.password && formik.errors.password
+                    }
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                  />
 
-                <Form.Control.Feedback type="invalid">
-                  {formik.errors.password}
-                </Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    {formik.errors.password}
+                  </Form.Control.Feedback>
+                </FloatingLabel>
               </Form.Group>
               <Form.Group className="mt-3">
-                <Form.Control
-                  placeholder={t("signin.confirm_password")}
-                  name="confirm_password"
-                  required
-                  id="confirm_password"
-                  type="password"
-                  isInvalid={
-                    formik.touched.confirm_password &&
-                    formik.errors.confirm_password
-                  }
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  value={formik.values.confirm_password}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {formik.errors.confirm_password}
-                </Form.Control.Feedback>
+                <FloatingLabel
+                  label={t("signin.confirm_password")}
+                  controlId="confirm_password"
+                >
+                  <Form.Control
+                    placeholder={t("signin.confirm_password")}
+                    name="confirm_password"
+                    required
+                    type="password"
+                    isInvalid={
+                      formik.touched.confirm_password &&
+                      formik.errors.confirm_password
+                    }
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values.confirm_password}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {formik.errors.confirm_password}
+                  </Form.Control.Feedback>
+                </FloatingLabel>
               </Form.Group>
               <Button className="w-100 mt-3" type="submit" variant="primary">
                 {t("signin.submit")}
